0573e050db2c169cfc2181c2aeec95dff6c71131
carlosdrt1 <carlosdrt229@gmail.com>
2022-04-15 23:02:56 -0300

    ConnectionFactory criado

---
+++ src/br/com/ferias/jdbc/ConnectionFactory.java
@@ -0,0 +1,18 @@
+
+package br.com.ferias.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    
+    public Connection getConnection(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/BDVENDAS",
+                    "adm","123");
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+   
+}
---
+++ src/br/com/ferias/jdbc/TestConnection.java
@@ -0,0 +1,15 @@
+
+package br.com.ferias.jdbc;
+
+import javax.swing.JOptionPane;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+            new ConnectionFactory().getConnection();
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro na conexão: \n" +e);
+        }
+    }
+}
